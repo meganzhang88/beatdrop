784ba488734ed8d4272e62c9025be68962430020
Megan Zhang <megan.zhangg88@gmail.com>
2020-11-09 12:31:07 -0500

    commit

--- functions/index.js
+++ functions/index.js
@@ -8,35 +8,35 @@ admin.initializeApp();
 //
 exports.helloWorld = functions.https.onRequest((request, response) => {
   response.send("hello, from ady!");
- });
+});
 
- exports.getPosts = functions.https.onRequest((req, res) => {
-    admin.firestore().collection('posts').get()
-        .then(data => {
-            let posts = [];
-            data.forEach(doc => {
-                posts.push(doc.data());
-            });
-            return res.json(posts);
-        })
-        .catch(err => console.error(err));
- });
-
- exports.createPost = functions.https.onRequest((req, res) => {
-    const newPost = {
-        body: req.body.body,
-        username: req.body.username,
-        createdAt: admin.firestore.Timestamp.fromDate(new Date())
-    };
-    admin
-        .firestore()
-        .collection('posts')
-        .add(newPost)
-        .then((doc) => {
-            res.json({ message: `document ${doc.id} created successfully`});
-        })
-        .catch((err) => {
-            res.status(500).json({ error: 'something went wrong'});
-            console.error(err);
+exports.getPosts = functions.https.onRequest((req, res) => {
+admin.firestore().collection('posts').get()
+    .then((data) => {
+        let posts = [];
+        data.forEach((doc) => {
+            posts.push(doc.data());
         });
- });
+        return res.json(posts);
+    })
+    .catch((err) => console.error(err));
+});
+
+exports.createPost = functions.https.onRequest((req, res) => {
+const newPost = {
+    body: req.body.body,
+    username: req.body.username,
+    createdAt: admin.firestore.Timestamp.fromDate(new Date())
+};
+admin
+    .firestore()
+    .collection('posts')
+    .add(newPost)
+    .then((doc) => {
+        res.json({ message: `document ${doc.id} created successfully`});
+    })
+    .catch((err) => {
+        res.status(500).json({ error: 'something went wrong'});
+        console.error(err);
+    });
+});
