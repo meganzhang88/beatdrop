ad2232d1aeb3e55d46c7eff5c6d8f1ad85b8a6aa
Michael Beebe <mpb9@live.unc.edu>
2020-11-13 17:32:13 -0500

    hayhayhayy

--- functions/index.js
+++ functions/index.js
@@ -39,24 +39,68 @@ app.get('/posts', (req, res) => {
     .catch((err) => console.error(err));
 })
 
-app.post('/post', (req, res) => {
+const FBAuth = (req, res, next) => {
+    let idToken;
+    if(req.headers.authorization && req.headers.authorization.startsWith('Bearer ')){
+        idToken = req.headers.authorization.split('Bearer ')[1];
+    } else {
+        console.error('No token found')
+        return res.status(403).json({ error: 'Unauthorized'});
+    }
+    admin.auth().verifyIdToken(idToken)
+        .then(decodedToken => {
+            req.user = decodedToken;
+            console.log(decodedToken);
+            return db.collection('users')
+                .where('userId', '==', req.user.uid)
+                .limit(1)
+                .get();
+        })
+        .then(data => {
+            req.user.handle = data.docs[0].data().handle;
+            return next();
+        })
+        .catch(err => {
+            console.error('Error while verifying token ', err);
+            return res.status(403).json(err);
+        })
+}
 
-const newPost = {
-    body: req.body.body,
-    username: req.body.username,
-    createdAt: new Date().toISOString()
-};
-db
-    .collection('posts')
-    .add(newPost)
-    .then((doc) => {
-        res.json({ message: `document ${doc.id} created successfully`});
-    })
-    .catch((err) => {
-        res.status(500).json({ error: 'something went wrong'});
-        console.error(err);
-    });
+app.post('/post', FBAuth, (req, res) => {
+    if (req.body.body.trim() === '') {
+        return res.status(400).json({ body: 'Body must not be empty'});
+    }
+
+
+    const newPost = {
+        body: req.body.body,
+        username: req.user.handle,
+        createdAt: new Date().toISOString()
+    };
+   
+    db
+        .collection('posts')
+        .add(newPost)
+        .then((doc) => {
+            res.json({ message: `document ${doc.id} created successfully`});
+        })
+        .catch((err) => {
+            res.status(500).json({ error: 'something went wrong'});
+            console.error(err);
+        });
 });
+
+const isEmail = (email) => {
+    const regEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if(email.match(regEx)) return true;
+    else return false;
+}
+
+const isEmpty = (string) => {
+    if(string.trim() === '') return true;
+    else return false;
+}
+
 
 // sign up route
 app.post('/signup', (req, res) => {
@@ -67,7 +111,22 @@ app.post('/signup', (req, res) => {
         handle: req.body.handle,
     };
 
-    // validate data
+    let errors = {};
+
+    if(isEmpty(newUser.email)){
+        errors.email = 'Must not be empty'
+    } else if(!isEmail(newUser.email)){
+        errors.email = 'Must be a valid email address'
+    }
+
+    if(isEmpty(newUser.password)) errors.password = 'Must not be empty';
+    if(newUser.password !== newUser.confirmPassword) errors.confirmPassword = 'Passwords must match';
+    if(isEmpty(newUser.handle)) errors.handle = 'Must not be empty';
+
+    if(Object.keys(errors).length > 0) return res.status(400).json(errors);
+
+    // TODO: validate data
+
 let token;
 let userId;
 
@@ -107,4 +166,33 @@ let userId;
         }
     })
 });
+
+app.post('/login', (req, res) => {
+    const user = {
+        email: req.body.email,
+        password: req.body.password
+    };
+
+    let errors = {};
+
+    if(isEmpty(user.email)) errors.email = 'Must not be empty';
+    if(isEmpty(user.password)) errors.password = 'Must not be empty';
+
+    if(Object.keys(errors).length > 0) return res.status(400).json(errors);
+
+    firebase.auth().signInWithEmailAndPassword(user.email, user.password)
+        .then(data => {
+            return data.user.getIdToken();
+        })
+        .then(token => {
+            return res.json({token});
+        })
+        .catch(err => {
+            console.error(err);
+            if(err.code === 'auth/wrong-password'){
+                return res.status(403).json( {general: 'Wrong credentials, please try again'});
+            } else return res.status(500).json({ error: err.code });
+        })
+})
+
 exports.api = functions.https.onRequest(app);
