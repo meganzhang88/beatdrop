f0b6d028193e3d3b49b5dd792008e8c2adfee9c0
Michael Beebe <mpb9@live.unc.edu>
2020-11-13 18:46:40 -0500

    lets gooo

---
+++ functions/util/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    apiKey: "AIzaSyBllU5j-_5NQpgSD6305_bA4bcKn2Jf1YI",
+    authDomain: "beatdrop-637f3.firebaseapp.com",
+    databaseURL: "https://beatdrop-637f3.firebaseio.com",
+    projectId: "beatdrop-637f3",
+    storageBucket: "beatdrop-637f3.appspot.com",
+    messagingSenderId: "121046094806",
+    appId: "1:121046094806:web:3710b88480aef3b9ca4a77",
+    measurementId: "G-KJ8ZCFREJS"
+};
